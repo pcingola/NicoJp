165cbdc8d95e01b493286d3130b9e1e33d4aaf3f
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-02-19 10:51:38 -0500

    Project updated

--- src/python/quiebra.py
+++ src/python/quiebra.py
@@ -53,26 +53,36 @@ def extract(l, se):
     return l[se[0]:se[1]].strip()
 
 
+def parse_commnad_line_args():
+    """
+    Parse command line arguments
+    """
+    parser = argparse.ArgumentParser(description='Process input "Quibra" formatted files.')
+    parser.add_argument('-i', '--infile', metavar='infile.txt', type=str, help='Input (txt) file', default=in_file)
+    parser.add_argument('-o', '--outfile', metavar='outfile.txt', type=str, help='Output file', default=out_file)
+    args = parser.parse_args()
+    return (args.infile, args.outfile)
+
+
+def process(in_file, out_file):
+    """
+    Process: Parse input file and create output file
+    Args:
+      in_file  : Input file name (file in 'Quiebra' format)
+      out_file : Output file name (writen as tab-separated TXT file)
+    """
+    # Open file and read lines
+    with open(in_file) as fin, open(out_file, 'w') as fout:
+        # Read file and get a list of lines
+        lines = fin.readlines()
+
+        # Process each line, convert to output format and print result
+        for l in lines:
+            lout = convert(l)
+            print(lout)
+            fout.write(lout + '\n')
 # ---
 # Main
 # ---
-
-parser = argparse.ArgumentParser(description='Process input "Quibra" formatted files.')
-parser.add_argument('-infile', metavar='infile.txt', type=str, help='Input (txt) file', default=in_file)
-parser.add_argument('-outfile', metavar='outfile.txt', type=str, help='Output file', default=out_file)
-
-args = parser.parse_args()
-in_file = args.infile
-out_file = args.outfile
-print("Input file  : '%0s'\nOutput file : '%1s'" % (in_file, out_file))
-
-# Open file and read lines
-with open(in_file) as fin, open(out_file, 'w') as fout:
-    # Read file and get a list of lines
-    lines = fin.readlines()
-
-    # Process each line, convert to output format and print result
-    for l in lines:
-        lout = convert(l)
-        print(lout)
-        fout.write(lout + '\n')
+(in_file, out_file) = parse_commnad_line_args()  # Parse command line
+process(in_file, out_file)  # Process file (parse input and write output file)
