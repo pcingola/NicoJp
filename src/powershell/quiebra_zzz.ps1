33de16d00b740305fe42f0cc0f963984f6b185be
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2018-04-06 17:46:37 -0400

    Added new 'quiebra' for tab separated files

--- src/powershell/quiebra_zzz.ps1
+++ src/powershell/quiebra_zzz.ps1
@@ -6,24 +6,15 @@ $inFile = "in.txt"
 $outFile = "out.txt"
 
 # Field numbers
-# TODO: This should be in a configuration file?
-$pageStart = 2
-$pageEnd = 6
-
-$montoUsdStart = 25
-$montoUsdEnd = 42
-
-$nameStart = 140
-$nameEnd = 189
-
-$paisStart = 602
-$paisEnd = 604
-
-$swiftStart = 700
-$swiftEnd = 710
-
-$cuentaStart = 850
-$cuentaEnd = 865
+$nameField = 1
+$currencyField = 3
+$amountField = 4
+$countryField = 5
+$bankField = 6
+$ibanField = 7
+$swiftField = 8
+$abaField = 9
+$accountField = 10
 
 #---
 # Main
@@ -32,25 +23,27 @@ $cuentaEnd = 865
 # Read each line form input file
 $dict = @{}
 foreach($line in Get-Content $inFile) {
-    if($line.Length -gt $cuentaEnd){
+	$fields = $line.Split("`t")
+    if($fields.Length -gt $accountField){
 		# Parse lines
-		$page = ($line[$pageStart..$pageEnd] -join '').Trim()
-		$montoUsd = $line[$montoUsdStart..$montoUsdEnd] -join ''
-		$name = $line[$nameStart..$nameEnd] -join ''
-		$pais = $line[$paisStart..$paisEnd] -join ''
-		$swift = $line[$swiftStart..$swiftEnd] -join ''
-		$cuenta = $line[$cuentaStart..$cuentaEnd] -join ''
+		$name = $field[nameField].Trim()
+		$currency = $field[currencyField].Trim()
+		$amount = $field[amountField].Trim()
+		$country = $field[countryField].Trim()
+		$bank = $field[bankField].Trim()
+		$iban = $field[ibanField].Trim()
+		$swift = $field[swiftField].Trim()
+		$aba = $field[abaField].Trim()
+		$account = $field[accountField].Trim()
 
 		# Prepend '/'
-		$cuenta = "/$cuenta"
-
-		# TODO: Write propper format checking for each field
+		$account = "/$account"
 
 		# Create output line
-		$outLine = "`"$page`t$montoUsd`t$name`t$pais`t$swift`t$cuenta"
+		$outLine = "`"$name`t4$currency`t$amount`t$country`t$bank`t$iban`t$swift`t$aba`t$account"
 
 		# Add to dictionary to avoid repeated lines
-		$dict[$page] = $outLine
+		$dict[$name] = $outLine
     }
 }
 
